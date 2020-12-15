8a9972f2583fe72399a1afb81f809dc6e06ea20c
Mahziar <seyedmahziar59@gmail.com>
2020-12-15 23:34:29 +0330

    a folder named tkinter made and a dir and a py are in it test_1 is for showing array

---
+++ tkinter/test_1.py
@@ -0,0 +1,16 @@
+import numpy
+a = list(numpy.arange(1, 10.5, .5))
+del numpy
+
+print(a)
+b = input(" ------> ")
+# false is not true for sure
+
+b = int(b)
+
+
+b += 15
+b *= 8
+
+print(b)
+input()
